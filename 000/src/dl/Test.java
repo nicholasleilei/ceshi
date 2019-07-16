8eea2086a2bec45436891c2eea2a1d9a01c90343
'nicholasleilei' <349417084@qq.com>
2019-07-16 23:42:24 +0800

    fenzhi01

--- 000/src/dl/Test.java
+++ 000/src/dl/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("∑÷÷ß01");
+System.out.println("kaifa0101001010010101");
 	}
 
 }
