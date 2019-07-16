1ca32e064e336405ac15e6ed24c63833273961c2
'nicholasleilei' <349417084@qq.com>
2019-07-16 15:35:11 +0800

    5

--- 000/src/dl/Test.java
+++ 000/src/dl/Test.java
@@ -5,10 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Hello123459890");
-
-System.out.println("Hello12345");
-
+System.out.println("Hello1234598900");
 	}
 
 }
