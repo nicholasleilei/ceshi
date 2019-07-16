0cfbb8c1a6ac95e2938357e26e7b8eca0ef75860
雷蕾 <51227402+nicholasleilei@users.noreply.github.com>
2019-07-16 23:16:28 +0800

    Update Test.java

--- 000/src/dl/Test.java
+++ 000/src/dl/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello123456666666s我是用户1");
+System.out.println("Hello123456666666s脦脪脢脟脫脙禄搂1闅忔椂闅忓湴66");
+		
 	}
 
 }
