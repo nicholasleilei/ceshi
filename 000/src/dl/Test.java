9ef4308064d8cfde543e7b84213fd1ff929b85a3
nicleilei <2760083777@qq.com>
2019-07-16 14:52:31 +0800

    12
    Signed-off-by: nicleilei <2760083777@qq.com>

--- 000/src/dl/Test.java
+++ 000/src/dl/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellosss1s23466633665s53ssss");
+System.out.println("Hello12345rrrrrrrrrrrrrrrr");
 	}
 
 }
